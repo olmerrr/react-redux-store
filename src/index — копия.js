c5663af21391f0d0a75b338081a5bab0965cade2
Yaroslav <dombrovskiy91@gmail.com>
2020-11-04 19:37:43 +0200

    testing Index

---
+++ src/index — копия.js
@@ -0,0 +1,23 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import {Provider} from "react-redux";
+import {BrowserRouter as Router} from "react-router-dom";
+import App from './components/app';
+import ErrorBoundry from "./components/error-boundry";
+import GamestoreService from "./services/gamestore-service";
+import {GamestoreServiceProvider} from "./components/gamestore-service-context";
+
+import store from "./store";
+
+const gamestoreService = new GamestoreService();
+ReactDOM.render(
+    <Provider store = {store}>
+        <ErrorBoundry>
+            <GamestoreServiceProvider value = {gamestoreService}>
+              <Router>
+                  <App/>
+              </Router>
+            </GamestoreServiceProvider>
+        </ErrorBoundry>
+    </Provider>, document.getElementById('root')
+);
